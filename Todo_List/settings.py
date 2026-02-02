599a7ac0325b9f6dc3f2993316002d2ce90f48e0
Muju-YT <muzammil00056@gmail.com>
2026-02-02 18:21:01 +0530

    fifth commit

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.ClickjackingMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 
